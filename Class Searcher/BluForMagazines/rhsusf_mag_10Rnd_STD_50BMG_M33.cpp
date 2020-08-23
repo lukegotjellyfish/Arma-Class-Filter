7c72afb7b2702ad1c5a3ece8aa70efa4c71ce827
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-08-24 00:51:35 +0100

    Create rhsusf_mag_10Rnd_STD_50BMG_M33.cpp

---
+++ Class Searcher/BluForMagazines/rhsusf_mag_10Rnd_STD_50BMG_M33.cpp
@@ -0,0 +1,11 @@
+rhsusf_mag_10Rnd_STD_50BMG_M33
+initSpeed=853;
+hit=25.307501;
+indirectHit=0;
+indirectHitRange=0;
+caliber=1.0253299;
+typicalSpeed=838.20001;
+airFriction=-0.00061699998;
+timeToLive=10;
+deflecting=15;
+count=30;
